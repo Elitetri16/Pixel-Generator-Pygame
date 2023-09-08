c3fa8f7655d806ac3c60640d8198524cacf32064
Samanwita Pal <109304180+Elitetri16@users.noreply.github.com>
2023-09-08 13:50:51 +0530

    Update pixel-generator.py

--- pixel-generator.py
+++ pixel-generator.py
@@ -10,8 +10,8 @@ GRID_WIDTH = 25  # Number of pixel blocks in width
 GRID_HEIGHT = 25  # Number of pixel blocks in height
 WIDTH = GRID_SIZE * GRID_WIDTH
 HEIGHT = GRID_SIZE * GRID_HEIGHT
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Pixel Art Generator")
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))  # Create the game window
+pygame.display.set_caption("Pixel Art Generator")  # Set the window caption
 
 # Colors
 BLACK = (0, 0, 0)
@@ -21,48 +21,49 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
 # Initialize the grid
-grid = [[WHITE for _ in range(GRID_WIDTH)] for _ in range(GRID_HEIGHT)]
+grid = [[WHITE for _ in range(GRID_WIDTH)] for _ in range(GRID_HEIGHT)]  # Create a 2D grid filled with white
 
 def draw_grid():
     for y in range(GRID_HEIGHT):
         for x in range(GRID_WIDTH):
-            pygame.draw.rect(WIN, grid[y][x], (x * GRID_SIZE, y * GRID_SIZE, GRID_SIZE, GRID_SIZE))
+            pygame.draw.rect(WIN, grid[y][x], (x * GRID_SIZE, y * GRID_SIZE, GRID_SIZE, GRID_SIZE))  # Draw grid cells
 
 def main():
-    clock = pygame.time.Clock()
-    drawing = False
-    current_color = BLACK
+    clock = pygame.time.Clock()  # Create a clock object to control frame rate
+    drawing = False  # Flag to indicate whether the mouse is being held down for drawing
+    current_color = BLACK  # Current drawing color is initialized to black
 
-    while True:
-        for event in pygame.event.get():
+    while True:  # Main game loop
+        for event in pygame.event.get():  # Check for events (e.g., mouse clicks, key presses, window close)
             if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+                pygame.quit()  # Quit pygame
+                sys.exit()  # Exit the script
 
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                drawing = True
+            if event.type == pygame.MOUSEBUTTONDOWN:  # Mouse button is pressed
+                drawing = True  # Start drawing
 
-            if event.type == pygame.MOUSEBUTTONUP:
-                drawing = False
+            if event.type == pygame.MOUSEBUTTONUP:  # Mouse button is released
+                drawing = False  # Stop drawing
 
-            if drawing and event.type == pygame.MOUSEMOTION:
-                x, y = event.pos
-                grid[y // GRID_SIZE][x // GRID_SIZE] = current_color
+            if drawing and event.type == pygame.MOUSEMOTION:  # Mouse is moving while drawing
+                x, y = event.pos  # Get the mouse position
+                grid[y // GRID_SIZE][x // GRID_SIZE] = current_color  # Update the color of the corresponding grid cell
 
-        keys = pygame.key.get_pressed()
+        keys = pygame.key.get_pressed()  # Get a list of keys that are currently pressed
         if keys[pygame.K_r]:
-            current_color = RED
+            current_color = RED  # Change drawing color to red when 'r' key is pressed
         elif keys[pygame.K_g]:
-            current_color = GREEN
+            current_color = GREEN  # Change drawing color to green when 'g' key is pressed
         elif keys[pygame.K_b]:
-            current_color = BLUE
+            current_color = BLUE  # Change drawing color to blue when 'b' key is pressed
         elif keys[pygame.K_w]:
-            current_color = BLACK
+            current_color = BLACK  # Change drawing color to black when 'w' key is pressed
 
-        draw_grid()
-        pygame.display.update()
-        clock.tick(60)
+        draw_grid()  # Draw the grid on the screen
+        pygame.display.update()  # Update the display
+        clock.tick(60)  # Limit the frame rate to 60 FPS
 
 if __name__ == "__main__":
-    main()
+    main()  # Run the main function if this script is executed
+
 
